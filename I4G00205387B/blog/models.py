df75b5d1a74f12c1478f46f4a1d5fb1873aa12fa
Chidubem Akpu <stanleychidubemakpu@gmail.com>
2022-06-22 13:55:24 +0100

    my django task

--- I4G00205387B/blog/models.py
+++ I4G00205387B/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Author = models.ForeignKey(get_user_model(),on_delete=models.CASCADE,)
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField()
+
+
+
